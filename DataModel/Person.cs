336c96d400d49269793239be07ce9531d6c0b171
Sultonazar Mamadazizov <sultonazar@outlook.com>
2018-01-24 20:55:16 +0200

    Изменил БД. Ввел ограничения и дату зачисления в штат сотрудника

--- DataModel/Person.cs
+++ DataModel/Person.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +11,24 @@ namespace TestAppSysTech
     public class Person
     {
         public int Id { get; set; }
+
+        [MaxLength(40)]
         public string Name { get; set; }
-        public double BaseSalary { get; set; }
+
+        public double BaseSalary{ get; set; }
+
+        [DefaultValue("false")]
         public bool IsRoot { get; set; }
-        public bool Supervisor { get; set; }
+
+        public string Supervisor { get; set; }
+
+        [MaxLength(40)]
         public string Login { get; set; }
+
+        [MaxLength(40)] [MinLength(6)]
         public string Password { get; set; }
+
+        public DateTime DateOfStart { get; set; }
 
         public int? GroupId { get; set; }
         public Group Group { get; set; }
